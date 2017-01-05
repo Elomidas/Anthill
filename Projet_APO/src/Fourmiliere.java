927050463917a2f525544f0e4a71fbfcff726364
tardy_martial <pp.btsig@free.fr>
2017-01-05 11:36:26 +0100

    Constructeur Fourmilière

--- Projet_APO/src/Fourmiliere.java
+++ Projet_APO/src/Fourmiliere.java
@@ -1,6 +1,19 @@
 /**
  * Created by Martial TARDY on 05/01/2017.
  */
-public class Fourmiliere extends Case {
+public class Fourmiliere extends Case{
+    private double m_nourr;
 
+    public Fourmiliere(int m_abcisse, int m_ordonnee, double m_pheromone, Case[] m_case_adj, double m_nourr) {
+        super(m_abcisse, m_ordonnee, m_pheromone, m_case_adj);
+        this.m_nourr = m_nourr;
+    }
+
+    public double getM_nourr() {
+        return m_nourr;
+    }
+
+    public void setM_nourr(double m_nourr) {
+        this.m_nourr = m_nourr;
+    }
 }
