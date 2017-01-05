563e80f78c88930892742cf997b4e0794316fd98
tardy_martial <pp.btsig@free.fr>
2017-01-05 11:29:03 +0100

    COnstructeur setGet Fourmi

--- Projet_APO/src/Fourmi.java
+++ Projet_APO/src/Fourmi.java
@@ -2,4 +2,32 @@
  * Created by Martial TARDY on 05/01/2017.
  */
 public class Fourmi {
+
+    protected int m_chemin[];
+    protected double m_nourr_transp;
+    protected enum m_etat {AVANCE , ARRET, TRANSPORTE } ;
+
+
+
+    public Fourmi(int[] m_chemin, double m_nourr_transp) {
+        this.m_chemin = m_chemin;
+        this.m_nourr_transp = m_nourr_transp;
+        m_etat m_e = m_etat.ARRET;
+    }
+
+    public int[] getM_chemin() {
+        return m_chemin;
+    }
+
+    public void setM_chemin(int[] m_chemin) {
+        this.m_chemin = m_chemin;
+    }
+
+    public double getM_nourr_transp() {
+        return m_nourr_transp;
+    }
+
+    public void setM_nourr_transp(double m_nourr_transp) {
+        this.m_nourr_transp = m_nourr_transp;
+    }
 }
