4cb75af395682138c0855d04bef67042502c8b31
tardy_martial <pp.btsig@free.fr>
2017-01-05 11:40:15 +0100

    Constructeur FourmiSenseur

--- Projet_APO/src/FourmiSenseur.java
+++ Projet_APO/src/FourmiSenseur.java
@@ -2,4 +2,19 @@
  * Created by Martial TARDY on 05/01/2017.
  */
 public class FourmiSenseur extends Fourmi {
+
+    private int m_portee;
+
+    public FourmiSenseur(int[] m_chemin, double m_nourr_transp, int m_portee) {
+        super(m_chemin, m_nourr_transp);
+        this.m_portee = m_portee;
+    }
+
+    public int getM_portee() {
+        return m_portee;
+    }
+
+    public void setM_portee(int m_portee) {
+        this.m_portee = m_portee;
+    }
 }
