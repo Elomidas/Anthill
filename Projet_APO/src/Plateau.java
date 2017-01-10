c5e32d04d94c6d7984e255f1ce79043de6721ec5
tardy_martial <pp.btsig@free.fr>
2017-01-10 18:45:48 +0100

    Constructeur setter Getter plateau

--- Projet_APO/src/Plateau.java
+++ Projet_APO/src/Plateau.java
@@ -2,4 +2,21 @@
  * Created by Martial TARDY on 05/01/2017.
  */
 public class Plateau {
+
+    private Case[][] m_tabCase;
+
+    public Plateau(int h, int l)
+    {
+        this.m_tabCase = new Case[h][l];
+    }
+
+    public Case getM_tabCase(int h, int l) {
+        return m_tabCase[h][l];
+    }
+
+    public void setM_tabCase(Case m_tabCase, int h, int l) {
+        this.m_tabCase[h][l] = m_tabCase;
+    }
+
+
 }
