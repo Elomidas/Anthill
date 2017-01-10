8e7ac58c4a755408cec4a5ef6a7c694fc463fa6a
Victor REMOND <victor96@hotmail.fr>
2017-01-10 18:11:37 +0100

    merge...

--- Projet_APO/src/FourmiSenseur.java
+++ Projet_APO/src/FourmiSenseur.java
@@ -4,17 +4,35 @@
 public class FourmiSenseur extends Fourmi {
 
     private int m_portee;
+    protected final static int[] m_poids = new int[] {50, 20, 10, 5, 0};
 
-    public FourmiSenseur(int[] m_chemin, double m_nourr_transp, int m_portee) {
+    public FourmiSenseur(int[] m_chemin, double m_nourr_transp, int m_portee) 
+    {
         super(m_chemin, m_nourr_transp);
         this.m_portee = m_portee;
     }
 
-    public int getM_portee() {
+    public int getM_portee() 
+    {
         return m_portee;
     }
 
-    public void setM_portee(int m_portee) {
+    public void setM_portee(int m_portee) 
+    {
         this.m_portee = m_portee;
+    }
+    
+    public void ChoixDirection()
+    {
+    	int taille = 1 + (2 * m_portee);
+    	int[][] proba = new int[taille][taille];
+    	for(int i = 0; i < taille; i++)
+    	{
+    		for(int j = 0; j < taille; j++)
+    		{
+    			proba[i][j] = 0;
+    		}
+    	}
+    	
     }
 }
