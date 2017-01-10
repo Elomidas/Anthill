3db484f5e019f89e520054cb2567cefa310e2060
tardy_martial <pp.btsig@free.fr>
2017-01-10 18:55:29 +0100

    fonction afficher plateau

--- Projet_APO/src/Plateau.java
+++ Projet_APO/src/Plateau.java
@@ -10,6 +10,10 @@ public class Plateau {
         this.m_tabCase = new Case[h][l];
     }
 
+    public Case[][] getM_tabCase() {
+        return m_tabCase;
+    }
+
     public Case getM_tabCase(int h, int l) {
         return m_tabCase[h][l];
     }
@@ -18,5 +22,15 @@ public class Plateau {
         this.m_tabCase[h][l] = m_tabCase;
     }
 
-
+    public void Afficher()
+    {
+        Case [][] tabCase = getM_tabCase();
+        for (int i = 0; i < tabCase.length ; i++)
+        {
+            for (int j = 0; j < tabCase.length ; j++)
+            {
+                getM_tabCase(i,j).Afficher();
+            }
+        }
+    }
 }
