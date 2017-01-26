b6999409c6afd4d03cc936768487e215bf49098b
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2017-01-26 17:08:37 +0100

    .

---
+++ Projet_APO/src/Menu.java
@@ -0,0 +1,18 @@
+
+public class Menu {
+
+	private Simulation sim;
+	
+	public Menu()
+	{
+		sim = new Simulation();
+	}
+	
+	public void AffMenu()
+	{
+		System.out.println("Bienvenue dans Anthill !");
+		System.out.println("Voici la liste de toutes les cartes disponibles pour la simulation :");
+		
+		
+	}
+}
